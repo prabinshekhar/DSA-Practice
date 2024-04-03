54b7dcc7240b7cccc09013aa63e472382885c5f5
PRABIN SHEKHAR <86481923+prabinshekhar@users.noreply.github.com>
2024-04-03 17:40:19 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -111,6 +111,22 @@ public class LinkedList {
          System.out.print("Not Found");
      }
   }
+
+   public void reverse()
+    {
+     Node prev=null;
+     Node curr=head;
+     Node next=null;
+      while(curr!=null)
+      {
+        next=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr=next;
+      }
+      head=prev;
+    }
+  
     public static void main(String[] args) {
       LinkedList list = new LinkedList();
       list.add(1);
